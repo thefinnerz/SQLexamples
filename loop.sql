f330cf00d77da97f73f66edfff829df2d757d1d0
Fin Roy <fin.roy1@gmail.com>
2023-10-17 10:37:08 +0100

    looping through to add product ids

---
+++ loop.sql
@@ -0,0 +1,12 @@
+Delimiter //
+
+CREATE PROCEDURE dorepeat(p1 INT)
+       BEGIN
+         SET @x = 0;
+         REPEAT
+			UPDATE hero SET product_id = RAND()*(8-1)+1 WHERE hero_id = @x + 11;  
+			SET @x = @x + 1;
+         UNTIL @x > p1 END REPEAT;
+       END;
+       
+CALL dorepeat(10)
